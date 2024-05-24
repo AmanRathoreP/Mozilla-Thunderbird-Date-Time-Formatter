0379fbcdb106656f8b72889f2824122ebc6fe538
Aman <aman.proj.rel@gmail.com>
2024-05-24 16:02:05 +0530

    add backend to display relative date time

--- src/backend/custom_column.js
+++ src/backend/custom_column.js
@@ -4,6 +4,25 @@ const { ThreadPaneColumns } = ChromeUtils.importESModule("chrome://messenger/con
 
 const ids = [];
 
+// in milliseconds
+var units = {
+  year: 24 * 60 * 60 * 1000 * 365,
+  month: 24 * 60 * 60 * 1000 * 365 / 12,
+  day: 24 * 60 * 60 * 1000,
+  hour: 60 * 60 * 1000,
+  minute: 60 * 1000,
+  second: 1000
+}
+
+var rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' })
+
+var get_relative_date_time = (d1, d2 = new Date()) => {
+  var elapsed = d1 - d2
+  for (var u in units)
+    if (Math.abs(elapsed) > units[u] || u == 'second')
+      return rtf.format(Math.round(elapsed / units[u]), u)
+}
+
 var customColumns = class extends ExtensionCommon.ExtensionAPI {
   getAPI(context) {
     context.callOnClose(this);
@@ -13,7 +32,8 @@ var customColumns = class extends ExtensionCommon.ExtensionAPI {
           ids.push(id);
 
           function get_addon_provided_relative_date_time(message) {
-            //todo
+            // nano seconds to milliseconds
+            return get_relative_date_time(new Date(message.date / 1000));
           }
 
           function getEmpty(message) {
@@ -27,7 +47,7 @@ var customColumns = class extends ExtensionCommon.ExtensionAPI {
             hidden: false,
             icon: false,
             resizable: true,
-            sortable: true,
+            sortable: false,
             textCallback: callback
           });
         },
